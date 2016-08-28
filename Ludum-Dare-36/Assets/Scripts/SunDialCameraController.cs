48e9b02d5ecf7fea3e7e16e1ef1e43a4e272da2a
Patrick Burns <pburns96@live.com>
2016-08-28 17:33:52 -0400

    Did things

--- Ludum-Dare-36/Assets/Scripts/SunDialCameraController.cs
+++ Ludum-Dare-36/Assets/Scripts/SunDialCameraController.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tizen;
 
 public class SunDialCameraController : MonoBehaviour
 {
     private Camera MainCamera;
     private Camera ThisCamera;
+    private Camera Minimap;
+    private GameObject Canvas;
     private EventManager manager;
+    private bool YouWinGame=false;
+    private Vector3 OldPosition;
+    private Vector3 NewPosition;
+    private GameObject YouWinTxt;
     
     [SerializeField]
     GameObject Player;
@@ -18,8 +29,12 @@ public class SunDialCameraController : MonoBehaviour
 
     // Use this for initialization
     void Start ()
-	{
-        
+    {
+        YouWinTxt = GameObject.Find("YouWinText");
+        YouWinTxt.transform.position = new Vector3(Screen.width/2, Screen.height/2,0);
+        YouWinTxt.gameObject.SetActive(false);
+        Canvas = GameObject.Find("Canvas");
+        Minimap = GameObject.Find("MiniMap").GetComponent<Camera>();
 	    ThisCamera = GetComponent<Camera>();
 	    ThisCamera.enabled = false;
 	    MainCamera = Player.GetComponent<Camera>();
@@ -35,8 +50,19 @@ public class SunDialCameraController : MonoBehaviour
     }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+	    if (YouWinGame && gameObject.transform.position.y <100)
+	    {
+	        OldPosition = gameObject.transform.position;
+	        NewPosition = new Vector3(OldPosition.x,OldPosition.y += .05f,OldPosition.z);
+	        gameObject.transform.position = NewPosition;
+	    }
+	    else if (YouWinGame)
+	    {
+            Canvas.SetActive(true);
+            YouWinTxt.gameObject.SetActive(true);
+        }
+	   
 	}
 
     void PanToSunDial()     //this turns on the sundial camera
@@ -49,7 +75,25 @@ public class SunDialCameraController : MonoBehaviour
     void CameraSwitch()
     {
         Debug.Log("CameraSwitch");
+        Canvas.SetActive(!Canvas.active);
         MainCamera.enabled = !MainCamera.enabled;
         ThisCamera.enabled = !ThisCamera.enabled;
+    }
+
+    void YouWin()
+    {
+        Invoke("CameraSwitch", TimeSwitchToCameraInSeconds);   //Half a second it goes to camera
+        Invoke("CameraRiseTrue",6);
+    }
+
+    void CameraRiseTrue()
+    {
+        YouWinGame = true;
+        Invoke("ResetGame",5);
+    }
+
+    void ResetGame()
+    {
+        SceneManager.LoadScene(0);  //Loads this scene 
     }
 }
